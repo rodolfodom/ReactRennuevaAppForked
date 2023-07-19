a23a522b2fced53a5ff07f624f765d11a653f07a
dali <hadriandali@gmail.com>
2023-07-18 21:13:14 -0600

    Header Version 01

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
-import  Rennueva from '../assets/Rennueva.jpg';
-import '../styles/Header.css';
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/Header.css";
+import Rennueva from "../assets/Rennueva.jpg";
 
-const Header = () => {
-  return (
-    <header>
-      
-      <nav>
-        <img src={Rennueva} alt = "Rennueva" className='menu'  />
-        <ul>
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/about">Acerca de</a></li>
-          <li><a href="/contact">Contacto</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header className="header-container">
+			 <img src={Rennueva} alt = "Rennueva" className="logo-header" />
+				
+       
+			<nav ref={navRef}>
+               <a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
 
-export default Header;
+export default Navbar;
