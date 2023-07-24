e0ff3a3ee4edb8443527da9c5e5b420e99a90c58
dali <hadriandali@gmail.com>
2023-07-24 10:10:36 -0600

    Header SideBAr

--- src/containers/LayoutHeader.jsx
+++ src/containers/LayoutHeader.jsx
@@ -4,29 +4,20 @@ import { Sidebar } from '../components/SideBar';
 import styled from "styled-components";
 
 const Layout = ({ children }) => {
-	  const [ sidebarOpen, setSidebarOpen ] = React.useState(true);
-	return (
+	  	return (
 		<div className="Layout">
-			<Header />
-			<Container className={sidebarOpen ? "sidebarState active" : ""}>
-            <Sidebar 
-              sidebarOpen={sidebarOpen}
-              setSidebarOpen={setSidebarOpen}/>
-          </Container>
-			
+			<Header />			
 			{children}
 		</div>
 	);
 }
 const Container = styled.div`
-  display: grid;
-  grid-template-columns: 90px auto;
-  background: ${({ theme }) => theme.bgtotal};
-  transition:all 0.3s ;
-  &.active {
-    grid-template-columns: 300px auto;
-  }
-  color:${({theme})=>theme.text};
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  
 `;
 
 export default Layout;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,34 +1,52 @@
 
-import {TodoProvider} from '../context/index'; 
+import { TodoProvider } from '../context/index';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MenuUser } from '../pages/Users/MenuUser';
 import Layout from '../containers/LayoutHeader';
 import Login from '../pages/Login';
 import { Sidebar } from '../components/SideBar';
+import styled from "styled-components";
 
 import React from 'react';
 function App() {
+  const [ sidebarOpen, setSidebarOpen ] = React.useState(true);
+
 
   return (
-      <TodoProvider>
-        <BrowserRouter>
-        
-        <Layout>
-        
+    <TodoProvider>
+      <BrowserRouter>
+      <Layout>
+        <Container className={sidebarOpen ? "sidebarState active" : ""}>
+
+          <Sidebar
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+            <Routes>
+              <Route path="/" element={<MenuUser />} />
+              <Route path="/users" element={<MenuUser />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<h1>Not Found 404</h1>} />
+            </Routes>
           
-          <Routes>
-            <Route path="/" element={<MenuUser/>}/>
-            <Route path="/users" element={<MenuUser/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="*" element={<h1>Not Found 404</h1>}/>
-          </Routes>
+        </Container>
         </Layout>
-        </BrowserRouter>
-       
-      </TodoProvider>
-    
+
+      </BrowserRouter>
+
+    </TodoProvider>
+
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  
+`;
 
 
 export default App;
