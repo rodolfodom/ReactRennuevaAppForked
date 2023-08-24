3ff3642ac2992d768741aad60ee53eb1905275bd
dali <hadriandali@gmail.com>
2023-08-24 00:05:01 -0600

    Avance en la creacion de graficas para grupos

--- src/components/BarsChartGroup.js
+++ src/components/BarsChartGroup.js
@@ -11,7 +11,7 @@ import {
     Legend,
     Filler,
 } from 'chart.js';
-import React , {useState, useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 ChartJS.register(
@@ -25,55 +25,74 @@ ChartJS.register(
     Filler
 );
 
-var beneficios = [72, 56];
-var meses = ["Grupo02", "Grupo03"];
-
-var misoptions = {
-    responsive : true,
-    animation : false,
-    plugins : {
-        legend : {
-            display : false
-        }
-    },
-    scales : {
-        y : {
-            min : -25,
-            max : 100
-        },
-        x: {
-            ticks: { color: 'rgba(0, 220, 195)'}
-        }
-    }
-};
-
-var midata = {
-    labels: meses,
-    datasets: [
-        {
-            label: 'Plastico reciclado',
-            data: beneficios,
-            backgroundColor: 'rgba(0, 220, 195, 0.5)'
-        }
-    ]
-};
 
 export default function Bars() {
-    const [groups, setGroups] = useState([]);
+
+   
+    const [beneficio, setBeneficio] = useState([]);
+    const [grupos, setGrupos] = useState([]);
+
     useEffect(() => {
         // Realiza una petición GET a una URL específica
         axios
-            .get('http://127.0.0.1:8000/Rennueva/get-all-groups/')
+            .get('http://127.0.0.1:8000/Rennueva/get-all-users-groups/')
             .then(response => {
                 const data = response.data;
-                setGroups(data);
               
+                console.log("data#hjshjahjashjas$##$#$#$##$#$#$##$");
+                console.log(data.group_data[0].group);
+
+                for (let i = 0; i < data.group_data.length; i++) {
+
+                    meses.push(data.group_data[i].group);
+                    beneficios.push(data.group_data[i].user_count);
+                }
+                console.log("data#$##$#$#$adsdasdsad##$#$#$##$");
+                console.log(meses);
+                console.log(beneficios);
+                setBeneficio(beneficios);
+                setGrupos(meses);
 
             })
             .catch(error => {
                 console.error(error);
             });
     }, []);
+
+    var beneficios = [];
+    var meses = [];
+
+
+
+    var misoptions = {
+        responsive: true,
+        animation: false,
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                min: 0,
+                max: 100
+            },
+            x: {
+                ticks: { color: 'rgba(0, 220, 195)' }
+            }
+        }
+    };
+
+    var midata = {
+        labels: grupos,
+        datasets: [
+            {
+                label: 'Plastico reciclado',
+                data: beneficio ,
+                backgroundColor: 'rgba(0, 220, 195, 0.5)'
+            }
+        ]
+    };
 
     return <Bar data={midata} options={misoptions} />
 }
