31f5e95b6fefd42fbb17eeca9b275524b9c7e9fa
dali <hadriandali@gmail.com>
2024-02-24 18:52:16 -0600

    Actualizacion de

--- src/pages/Users/MenuUser.js
+++ src/pages/Users/MenuUser.js
@@ -1,28 +1,26 @@
-
 import React, { useState, useEffect, useContext } from "react";
-import '../../styles/user/MenuUser.css'
-import { TodoContext } from '../../context/index.js';
-import { ModalUser } from './ModalUser.js';
+import "../../styles/user/MenuUser.css";
+import { TodoContext } from "../../context/index.js";
+import { ModalUser } from "./ModalUser.js";
 import UserTable from "../../components/Table";
 import CUDButtons from "../../containers/CUDButtons";
 import BarsChart from "../../components/BarsChart";
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
-import Title from '../../components/Title';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Title from "../../components/Title";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
 
 function MenuUser() {
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -36,11 +34,11 @@ function MenuUser() {
   }));
 
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
     // hide last border
-    '&:last-child td, &:last-child th': {
+    "&:last-child td, &:last-child th": {
       border: 0,
     },
   }));
@@ -49,79 +47,86 @@ function MenuUser() {
     return { name, calories, fat, carbs, protein };
   }
 
-
   const [datos, setDatos] = useState([]);
-  const { textOpenModalText,totalListlUsers, openModalCreate, setOpenModalCreate, setOpenModalEdit,
-    openModalEdit, setOpenModalDelete, openModalDelete, openModalText, setOpenModalText } = useContext(TodoContext);
+  const {
+    textOpenModalText,
+    totalListlUsers,
+    openModalCreate,
+    setOpenModalCreate,
+    setOpenModalEdit,
+    openModalEdit,
+    setOpenModalDelete,
+    openModalDelete,
+    openModalText,
+    setOpenModalText,
+  } = useContext(TodoContext);
 
   useEffect(() => {
-
     setDatos(totalListlUsers);
   }, []);
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <CssBaseline />
 
         <Box
           component="main"
           sx={{
-            backgroundColor: (theme) =>
-              theme.palette.grey[100],
+            backgroundColor: (theme) => theme.palette.grey[100],
             flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
+            height: "100vh",
+            overflow: "auto",
           }}
         >
           <Toolbar />
-          <Container maxWidth="lg">
+          <Container maxWidth="xl"  sx={{ mb: 5 }}>
             <Grid container spacing={3}>
-
-              <Grid item xs={6}>
-
-                <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                  <Title >Usuarios</Title>
+              <Grid item xs={12}>
+                <Paper
+                  sx={{
+                    p: 3,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Title>Usuarios</Title>
                   <CUDButtons model="User" />
                   <Title>Usuarios Creados</Title>
                   <UserTable />
-
-
                 </Paper>
               </Grid>
-              <Grid item xs={12} md={12} lg={6}>
+              <Grid item xs={12}>
                 <Paper
                   sx={{
                     p: 4,
-                    display: 'flex',
-                    flexDirection: 'column',
+                    display: "flex",
+                    flexDirection: "column",
                     height: 580,
                   }}
                 >
                   <BarsChart />
-
                 </Paper>
-
               </Grid>
-              
             </Grid>
-            
           </Container>
 
           {openModalCreate && (
             <ModalUser mode={"CREAR"}>
               La funcionalidad de agregar TODO
-            </ ModalUser >
+            </ModalUser>
           )}
           {openModalEdit && (
             <ModalUser mode={"EDITAR"}>
               La funcionalidad de editar TODO
-            </ ModalUser >
+            </ModalUser>
           )}
           {openModalDelete && (
             <ModalUser mode={"BORRAR"}>
               La funcionalidad de borrar TODO
-            </ ModalUser >
+            </ModalUser>
           )}
 
           {openModalText && (
@@ -131,7 +136,9 @@ function MenuUser() {
               aria-labelledby="alert-dialog-title"
               aria-describedby="alert-dialog-description"
             >
-              <DialogTitle id="alert-dialog-title">{textOpenModalText}</DialogTitle>
+              <DialogTitle id="alert-dialog-title">
+                {textOpenModalText}
+              </DialogTitle>
               <DialogContent>
                 <DialogContentText id="alert-dialog-description">
                   {textOpenModalText}
@@ -142,13 +149,10 @@ function MenuUser() {
               </DialogActions>
             </Dialog>
           )}
-          
-
         </Box>
       </Box>
     </ThemeProvider>
   );
 }
-
 
 export { MenuUser };
