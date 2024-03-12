7158794e18708f62cd6ea24faff53d22c0e43e9e
dali <hadriandali@gmail.com>
2024-03-11 18:44:07 -0600

    tracking arreglado

--- src/pages/MenuTracking.jsx
+++ src/pages/MenuTracking.jsx
@@ -1,16 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"; // Importar useParams
+import React, { useState,   useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import '../styles/user/MenuUser.css';
+import { TodoContext } from '../context/index.js';
+// Importa los demás componentes y bibliotecas que necesitas...
+import { Document, Page } from 'react-pdf';
 import axios from 'axios';
 import { ThemeProvider, createTheme, Box, Grid, Paper, Container, Toolbar, CssBaseline, Button, TextField } from '@mui/material';
 import Title from '../components/Title.js';
 
 function MenuTracking() {
+   
     const [pdfFile, setPdfFile] = useState(null);
+    const [pdfFile2, setPdfFile2] = useState(null);
+    const defaultTheme = createTheme();
     const [url, setUrl] = useState(null);
-    const [folio, setFolio] = useState("");
+    const [url2, setURL2] = useState(null);
+    const [folio, setFolio] = useState(null);
+
+    function openPdfInNewWindow() {
+        const blob = base64ToBlob(pdfFile, 'application/pdf');
+        const url = URL.createObjectURL(blob);
+    
+        // Abrir el PDF en una nueva ventana
+        window.open(url, '_blank');
+    }
+    
     const { trackingNumber } = useParams(); // Usar useParams para obtener el número de tracking de la URL
 
-    const defaultTheme = createTheme();
+ 
 
     useEffect(() => {
         if (trackingNumber) {
@@ -18,6 +35,8 @@ function MenuTracking() {
             getPDF(trackingNumber); // Llamar a getPDF automáticamente si hay un número de tracking
         }
     }, [trackingNumber]); // El efecto se ejecuta cuando cambia trackingNumber
+
+    
 
     async function getPDF(trackingNumber) {
         try {
@@ -34,17 +53,20 @@ function MenuTracking() {
             console.log(error);
         }
     }
+    
 
     function base64ToBlob(base64, mimeType) {
         const base64Real = base64.split(',')[1] || base64;
+
         const byteCharacters = atob(base64Real);
         const byteNumbers = new Array(byteCharacters.length);
         for (let i = 0; i < byteCharacters.length; i++) {
-            byteNumbers[i] = byteCharacters.charCodeAt(i);
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
         }
         const byteArray = new Uint8Array(byteNumbers);
-        return new Blob([byteArray], { type: mimeType });
-    }
+        return new Blob([byteArray], {type: mimeType});
+      }
+      
 
     return (
         <ThemeProvider theme={defaultTheme}>
@@ -60,9 +82,9 @@ function MenuTracking() {
                     }}
                 >
                     <Toolbar />
-                    <Container maxWidth="lg">
+                    <Container maxWidth="xl">
                         <Grid container spacing={3}>
-                            <Grid item xs={12}>
+                            <Grid item xl >
                                 <Paper
                                     sx={{
                                         p: 3,
@@ -70,26 +92,39 @@ function MenuTracking() {
                                         flexDirection: 'column',
                                         alignItems: 'center',
                                         justifyContent: 'flex-start',
-                                        height: 'auto',
+                                        height: 'auto', // Modificado para ajustar el tamaño del contenedor
                                     }}
                                 >
                                     <Title>Tracking</Title>
+
                                     <TextField
                                         label="Clave de Responsiva"
                                         name="responsiva"
                                         required
                                         fullWidth
-                                        value={folio} // Usar value para que se rellene automáticamente
+                                        onChange={(e) => setFolio(e.target.value)}
+
                                     />
-                                    {/* Puedes optar por omitir este botón si la carga es completamente automática */}
                                     <Button 
                                         variant="contained" 
                                         color="primary" 
                                         sx={{ mt: 3, mb: 2 }}
-                                        onClick={() => getPDF(folio)}
+                                        onClick={getPDF}
+                                        
                                     >
                                         Buscar
                                     </Button>
+
+                                    {/* Visualizador de PDF */}
+                                    {pdfFile && (
+                                        <Document
+                                            file={url}
+                                
+                                            onLoadError={(error) => console.error('Error al cargar el PDF:', error)}
+                                        >
+                                            <Page pageNumber={1} />
+                                        </Document>
+                                    )}
                                 </Paper>
                             </Grid>
                         </Grid>
