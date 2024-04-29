c7bf9130f1c9a31e1f1429e0da00bf5413c86fbc
dali <hadriandali@gmail.com>
2024-04-29 13:35:02 -0600

    -Feat donor recolection table

--- src/components/boards/DonorRecolectionTable.jsx
+++ src/components/boards/DonorRecolectionTable.jsx
@@ -22,6 +22,281 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Collapse from "@mui/material/Collapse";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const [openCancelModal, setOpenCancelModal] = React.useState(false);
+  const [openEditModal, setOpenEditModal] = React.useState(false);
+  const [recolectionToEdit, setRecolectionToEdit] = React.useState(null);
+
+  const [clientes, setClientes] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const {
+    updateDonorInfo,
+    setUpdateDonorInfo,
+    setOpenModalText,
+    setTextOpenModalText,
+  } = useContext(TodoContext);
+
+
+
+  function createData(name, calories, fat, carbs, protein, price) {
+    return {
+      name,
+      calories,
+      fat,
+      carbs,
+      protein,
+      price,
+      history: [
+        {
+          date: '2020-01-05',
+          customerId: '11091700',
+          amount: 3,
+        },
+        {
+          date: '2020-01-02',
+          customerId: 'Anonymous',
+          amount: 1,
+        },
+      ],
+    };
+  }
+
+  const handleClickOpen = (id) => {
+    setOpenCancelModal(true);
+  }
+
+  const handleConfirmDelete = (user, id) => {
+    console.log("Borrado confirmado");
+    console.log(user);
+    console.log(id);
+
+    axios
+    .post(`${process.env.REACT_APP_API_URL}/delete-donor-recollection/`, {
+      user: user,
+      id_order: id 
+    })
+    .then((response) => {
+      console.log(response);
+      setOpen(false);
+      setOpenModalText(true);
+      setTextOpenModalText("Donación eliminada correctamente");
+      setUpdateDonorInfo(true);
+
+
+    })
+    .catch((error) => {
+      console.error(error);
+      setOpen(false);
+      setOpenModalText(true);
+      setTextOpenModalText("Error al eliminar la donación");
+    });
+
+
+    handleClose();
+  };
+
+  const handleClose = () => {
+    setOpenCancelModal(false);
+  };
+
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+  
+];
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>{row.id}</TableCell>
+                    <TableCell>{row.donador}</TableCell>
+                    <TableCell>{row.fecha}</TableCell>
+                    <TableCell>{row.direccion_completa}</TableCell>
+                    <TableCell>{row.peso_estimado}</TableCell>
+
+                    
+
+                    <TableCell>
+                      <Button
+                        color={
+                          row.status === "solicitado" ? "primary" : "error" 
+                        }
+                        onClick={() => {
+                          setRecolectionToEdit(row);
+                          setOpenEditModal(true);
+                        }}
+                      >
+                        <Edit />
+                      </Button>
+                    </TableCell>
+
+                    <TableCell>
+                      <IconButton
+                        aria-label="borrar"
+                        onClick={() => handleClickOpen(11)}
+                      >
+                        {" "}
+                        {/* Suponiendo que el ID del reporte es "1", aquí deberías pasar el ID real según tu lógica */}
+                        <DeleteIcon />
+                      </IconButton>
+                      <Dialog
+                        open={openCancelModal}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                      >
+                        <DialogTitle id="alert-dialog-title">
+                          {"¿Estás seguro de querer borrar?"}
+                        </DialogTitle>
+                        <DialogContent>
+                          <DialogContentText id="alert-dialog-description">
+                            Esta acción no se puede deshacer.
+                          </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                          <Button onClick={handleClose}>Cancelar</Button>
+                          <Button onClick={ () => handleConfirmDelete(row.donador, row.id) } autoFocus>
+                            {" "}
+                            {/* Aquí no necesitas pasar el ID ya que se maneja a través del estado */}
+                            Confirmar
+                          </Button>
+                        </DialogActions>
+                      </Dialog>
+                    </TableCell>
+                    <TableCell
+                      bgcolor={
+                        row.status === "solicitado"
+                          ? "#008000"
+                          : row.status === "pendienteRecoleccion"
+                          ? "#FFA500"
+                          : "#FF0000"
+                      }
+                      sx={{
+                        borderRadius: "10px", // Adjust the radius to your preference
+                        padding: "1px", // Example of adjusting padding
+                        marginRight: "10px", // Example of adjusting margin
+                        marginTop: "15px", // Example of adjusting margin  
+                        display: "flex", // Center the content horizontally and vertically
+                        justifyContent: "center", // Center the content horizontally
+                        width: "100px", // Adjust the width to your preference
+                        alignItems: "center", // Center the content horizontally and vertically
+                        color: "white", // Change the text color
+                        
+                        // Add more styles here
+                      }}
+                    >
+                      {row.status === "pendienteRecoleccion"
+                        ? "pendiente"
+                        : row.status}
+                    </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box bgcolor={"#f5f5f5"} sx={{ margin: 1, width : "100%" }}>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Datos de la recoleccion
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Horario preferente </TableCell>
+                    <TableCell>Peso estimado</TableCell>
+
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((historyRow) => (
+                    <TableRow key={historyRow.name}>
+                      <TableCell component="th" scope="row">
+                        {row.hora_preferente_recoleccion === "00:00:00" ? "No especificado" : row.hora_preferente_recoleccion} 
+                      </TableCell>
+                      <TableCell>{row.peso_estimado}</TableCell>
+                    
+                      
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Datos del donador
+              </Typography>
+
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Nombre</TableCell>
+                    <TableCell>Telefono</TableCell>
+                    <TableCell>Estado</TableCell>
+                    <TableCell>Ciudad</TableCell>
+                    <TableCell>Colonia</TableCell>
+                    <TableCell>Calle</TableCell>
+                    <TableCell>Num. Ext</TableCell>
+                    <TableCell>Num. Int</TableCell>
+                    <TableCell>Codigo Postal</TableCell>
+
+
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow key={row.donador}>
+                    <TableCell component="th" scope="row">
+                      {row.nombre}
+                    </TableCell>
+                    <TableCell>{row.telefono}</TableCell>
+                    
+                    <TableCell>{row.estado}</TableCell>
+                    <TableCell>{row.ciudad}</TableCell>
+                    <TableCell>{row.localidad}</TableCell>
+                    <TableCell>{row.calle}</TableCell>
+                    <TableCell>{row.num_ext}</TableCell>
+                    <TableCell>{row.num_int}</TableCell>
+                    <TableCell>{row.codigo_postal}</TableCell>
+
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+            </Box>
+
+          </Collapse>
+        </TableCell>
+      </TableRow>
+      <EditRecolectionModal
+        open={openEditModal}
+        setOpen={setOpenEditModal}
+        recolection={recolectionToEdit}
+        setMessage={setTextOpenModalText}
+        setOpenMessageModal={setOpenModalText}
+        update={updateDonorInfo}
+        setUpdate={setUpdateDonorInfo}
+      />
+    </React.Fragment>
+  );
+}
+
 
 const DonorRecolectionTable = () => {
   const [clientes, setClientes] = useState([]);
@@ -106,102 +381,26 @@ const DonorRecolectionTable = () => {
             <TableHead>
               <TableRow>
                 {/* Añade aquí tus encabezados de tabla */}
+                <TableCell></TableCell>
                 <TableCell>Id</TableCell>
                 <TableCell>Donador</TableCell>
-                <TableCell>Fecha</TableCell>
+                <TableCell>Fecha Solicitud</TableCell>
                 <TableCell>Direccion</TableCell>
                 <TableCell>Peso Estimado</TableCell>
-                <TableCell>Status</TableCell>
+                
                 <TableCell>Editar</TableCell>
                 <TableCell>Eliminar</TableCell>
+                <TableCell>Status</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {clientes
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((orden, index) => (
-                  <TableRow key={index}>
-                    {/* Añade aquí tus celdas de datos */}
-                    <TableCell>{orden.id}</TableCell>
-                    <TableCell>{orden.donador}</TableCell>
-                    <TableCell>{orden.fecha}</TableCell>
-                    <TableCell>{orden.direccion_completa}</TableCell>
-                    <TableCell>{orden.peso_estimado}</TableCell>
-
-                    <TableCell
-                      bgcolor={
-                        orden.status === "solicitado"
-                          ? "#008000"
-                          : orden.status === "pendienteRecoleccion"
-                          ? "#FFA500"
-                          : "#FF0000"
-                      }
-                      sx={{
-                        borderRadius: "10px", // Adjust the radius to your preference
-                        padding: "1px", // Example of adjusting padding
-                        marginTop: "13px", // Example of adjusting margin
-                        display: "flex", // Center the content horizontally and vertically
-                        justifyContent: "center", // Center the content horizontally
-                        width: "100px", // Adjust the width to your preference
-                        alignItems: "center", // Center the content horizontally and vertically
-                        color: "white", // Change the text color
-
-                        // Add more styles here
-                      }}
-                    >
-                      {orden.status === "pendienteRecoleccion"
-                        ? "pendiente"
-                        : orden.status}
-                    </TableCell>
-
-                    <TableCell>
-                      <Button
-                        color={
-                          orden.status === "solicitado" ? "primary" : "error" 
-                        }
-                        onClick={() => {
-                          setRecolectionToEdit(orden);
-                          setOpenEditModal(true);
-                        }}
-                      >
-                        <Edit />
-                      </Button>
-                    </TableCell>
-
-                    <TableCell>
-                      <IconButton
-                        aria-label="borrar"
-                        onClick={() => handleClickOpen(11)}
-                      >
-                        {" "}
-                        {/* Suponiendo que el ID del reporte es "1", aquí deberías pasar el ID real según tu lógica */}
-                        <DeleteIcon />
-                      </IconButton>
-                      <Dialog
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="alert-dialog-title"
-                        aria-describedby="alert-dialog-description"
-                      >
-                        <DialogTitle id="alert-dialog-title">
-                          {"¿Estás seguro de querer borrar?"}
-                        </DialogTitle>
-                        <DialogContent>
-                          <DialogContentText id="alert-dialog-description">
-                            Esta acción no se puede deshacer.
-                          </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                          <Button onClick={handleClose}>Cancelar</Button>
-                          <Button onClick={ () => handleConfirmDelete(orden.donador, orden.id) } autoFocus>
-                            {" "}
-                            {/* Aquí no necesitas pasar el ID ya que se maneja a través del estado */}
-                            Confirmar
-                          </Button>
-                        </DialogActions>
-                      </Dialog>
-                    </TableCell>
-                  </TableRow>
+                
+                    
+                    <Row key={index} row={orden} />
+                
                 ))}
             </TableBody>
           </Table>
